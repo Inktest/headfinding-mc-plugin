c217bab38a062d18222aaee77fdc099fe4c7717d
Rory Skipper <roryskipper123@gmail.com>
2022-11-18 10:08:20 -0700

    Lots of improvements, optimizations and bug fixes

--- src/heads/inktest/Main.java
+++ src/heads/inktest/Main.java
@@ -1,23 +1,26 @@
 package heads.inktest;
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
+
+import heads.inktest.util.SkinUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -27,119 +30,193 @@ import net.md_5.bungee.api.ChatColor;
 public class Main extends JavaPlugin implements Listener {
 
 	
-	public static boolean round;
-	public static Map<String,Integer> count = new HashMap<String,Integer>();
-	public static Map<String,Boolean> bcount = new HashMap<String,Boolean>();
-	public static Map<Location, String> headsLoc = new HashMap<Location, String>();
+	private boolean round = false;
+	private final Map<UUID,Integer>   count      = new HashMap<>();
+	private final Set<UUID>           headsFound = new HashSet<>();
+	private final Set<UUID>           headsPlaced = new HashSet<>();
+	private final Map<Location, UUID> headsLoc   = new HashMap<>();
+	
+	private ItemStack headItem;
+
+	private final String loreFlag = ChatColor.GRAY + "Hide your head as best you can from other players!";
 	
 	@Override
-	public void onEnable( ) {
-		System.out.println("Heads plugin loaded!");
-		round = false;
-		 Bukkit.getPluginManager().registerEvents(this, this);
+	public void onEnable() {
+		headItem = new ItemStack(Material.PLAYER_HEAD);
+		ItemMeta     meta = headItem.getItemMeta();
+		List<String> lore = new ArrayList<>();
+		lore.add(loreFlag);
+		meta.setLore(lore);
+		headItem.setItemMeta(meta);
+
+		Bukkit.getPluginManager().registerEvents(this, this);
 	}
-	
-	@EventHandler
-	public void blockPlace(BlockPlaceEvent event) {
-		if (!(round)) return;
-		ItemStack item = event.getItemInHand();
-		if (item.getType() == Material.PLAYER_HEAD && item.getItemMeta().getLore().contains(ChatColor.GRAY + "Hide your head as best you can from other players!")) {
-			headsLoc.put(event.getBlock().getLocation(), event.getPlayer().getName());
+
+	@Override
+	public void onDisable() {
+		clearHeads();
+	}
+
+	public void clearHeads() {
+		headsLoc.keySet().forEach(loc -> loc.getBlock().setType(Material.AIR));
+		Bukkit.getOnlinePlayers().forEach(player -> clearHeads(player));
+	}
+
+	public void clearHeads(Player player) {
+		PlayerInventory inv = player.getInventory();
+		for (int slot = 0; slot < inv.getSize(); slot++) {
+			ItemStack item = inv.getItem(slot);
+			if (item != null && item.getType() == Material.PLAYER_HEAD) {
+				ItemMeta meta = item.getItemMeta();
+				if (meta != null && meta.hasLore() && meta.getLore().contains(loreFlag)) {
+					inv.setItem(slot, null);
+				}
+			}
 		}
 	}
 	
-	@EventHandler
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void blockPlace(BlockPlaceEvent event) {
+		if (!round) return;
+
+		Player player = event.getPlayer();
+		ItemStack item = event.getItemInHand();
+		ItemMeta meta = item.getItemMeta();
+
+		if (item.getType() == Material.PLAYER_HEAD && meta != null && meta.hasLore() && meta.getLore().contains(loreFlag)) {
+			if (headsPlaced.contains(player.getUniqueId())) {
+				player.sendMessage(ChatColor.RED + "You've already hidden your head this round!");
+				return;
+			}
+
+			OfflinePlayer owningPlayer = ((SkullMeta) meta).getOwningPlayer();
+
+			if (owningPlayer == null || !owningPlayer.getUniqueId().equals(player.getUniqueId())) {
+				player.sendMessage(ChatColor.RED + "You've may only place your own head!");
+				return;
+			}
+
+			headsLoc.put(event.getBlock().getLocation(), event.getPlayer().getUniqueId());
+			headsPlaced.add(player.getUniqueId());
+
+			player.sendMessage(ChatColor.YELLOW + "Your head has been placed, soon the sides will be swapped and you need to collect as many heads as you can!");
+		}
+	}
+	
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
 	public void blockBreak(BlockBreakEvent event) {
 		if (!(round)) return;
+
 		Block block = event.getBlock();
+		if (block.getType() != Material.PLAYER_HEAD && block.getType() != Material.PLAYER_WALL_HEAD) return;
+
 		Location blockLoc = block.getLocation();
-		String name = headsLoc.get(blockLoc);
-		String bname = event.getPlayer().getName();
-		if (name == null) return;
-		if (name.equals(bname)) {
-			event.getPlayer().sendMessage(ChatColor.RED + "You cannot break your own head!");
+
+		UUID headUUID = headsLoc.get(blockLoc);
+		if (headUUID == null) return;
+
+		Player breaker = event.getPlayer();
+		UUID breakerUUID = breaker.getUniqueId();
+		if (breakerUUID.equals(headUUID)) {
+			breaker.sendMessage(ChatColor.RED + "You cannot break your own head!");
 			event.setCancelled(true);
 			return;
-			}
-		if (block.getType() != Material.PLAYER_HEAD && block.getType() != Material.PLAYER_WALL_HEAD) return;
-		Bukkit.broadcastMessage(ChatColor.YELLOW + bname + " has found " + name + "'s head!");
-		if (count.get(bname) == null) {
-			count.put(bname,0);
-			bcount.put(bname, true);
-		}
-		if (count.get(name) == null) {
-			count.put(name,0);
-			bcount.put(name, true);
 		}
 		
-		Integer current = count.get(bname);
-		current += 1;
-		count.put(bname, current);
-		Boolean bcurrent = bcount.get(name);
-		bcurrent = false;
-		bcount.put(name, bcurrent);
-		
+		count.put(breakerUUID, count.getOrDefault(breakerUUID, 0) + 1);
+		headsFound.add(headUUID);
 		headsLoc.remove(blockLoc);
+
+		// Make sure head dropped has special lore line so we can remove heads later
+		event.setDropItems(false);
+		ItemStack headToGive = headItem.clone();
+		SkullMeta meta = (SkullMeta) headToGive.getItemMeta();
+		OfflinePlayer offlinePlayer = Bukkit.getPlayer(headUUID);
+		if (offlinePlayer == null) offlinePlayer = Bukkit.getOfflinePlayer(headUUID);
+		meta.setOwningPlayer(offlinePlayer);
+		headToGive.setItemMeta(meta);
+		breaker.getInventory().addItem(headToGive);
+
+		Bukkit.broadcastMessage(ChatColor.YELLOW + event.getPlayer().getName() + ChatColor.GRAY + " has found " + ChatColor.YELLOW + SkinUtil.getName(headUUID) + "'s " + ChatColor.GRAY + "head!");
 	}
-	
+
+	@EventHandler
+	public void onQuit(PlayerQuitEvent event) {
+		clearHeads(event.getPlayer());
+	}
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (!(cmd.getName().equalsIgnoreCase("heads"))) return false;
+		if (!(cmd.getName().equalsIgnoreCase("headfinding"))) return false;
 		if (!(sender.hasPermission("heads.commands"))) {
 			sender.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
 			return true;
 		}
 		if (args.length == 0) {
-			sender.sendMessage(ChatColor.RED + "Usage: /heads [giveheads/start/stop/count]");
+			sender.sendMessage(ChatColor.RED + "Usage: /" + label + " [giveheads/start/stop/count]");
 			return true;
 		}
 		
 		switch(args[0].toLowerCase()) {
 		case "giveheads":
 			
-			for(Player player : Bukkit.getOnlinePlayers()){
-			    ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+			for (Player player : Bukkit.getOnlinePlayers()) {
+			    ItemStack head = headItem.clone();
 			    SkullMeta meta = (SkullMeta) head.getItemMeta();
 			    meta.setOwningPlayer(player);
-			    List<String> lore = new ArrayList<String>();
-			    lore.add(ChatColor.GRAY + "Hide your head as best you can from other players!");
-			    meta.setLore(lore);
 			    head.setItemMeta(meta);
-			    player.getInventory().addItem(head);		    
+
+				if (!player.getInventory().contains(head)) {
+					player.getInventory().addItem(head);
+				}
 			}
 			sender.sendMessage(ChatColor.YELLOW + "Heads given to all players!");
 			break;
 		case "start":
+			if (round) {
+				sender.sendMessage(ChatColor.RED + "The round has already started!");
+				return true;
+			}
+
 			round = true;
 			count.clear();
-			bcount.clear();
+			headsFound.clear();
+			headsLoc.clear();
+			headsPlaced.clear();
 			sender.sendMessage(ChatColor.YELLOW + "Round started!");
 			break;
 		case "stop":
-			round = false;
-			for (Map.Entry<Location, String> set : headsLoc.entrySet()) {
-				set.getKey().getBlock().setType(Material.AIR);
+			if (!round) {
+				sender.sendMessage(ChatColor.RED + "The round hasnt started yet!");
+				return true;
 			}
+
+			round = false;
+			clearHeads();
 			headsLoc.clear();
+			headsPlaced.clear();
 			sender.sendMessage(ChatColor.YELLOW + "Stopped round!");
 			break;
 		case "count":
-			Map<String,Integer> parsed = new HashMap<String,Integer>();
-			for (Map.Entry<String, Integer> set : count.entrySet()) {			
-				 parsed.put(set.getKey(), set.getValue()+(bcount.get(set.getKey())?1:0));
-			 }
-			Map<String,Integer> sorted = new TreeMap<>(parsed);
+			Map<UUID,Integer> parsed = new HashMap<>();
+			for (Map.Entry<UUID, Integer> set : count.entrySet()) {
+				 parsed.put(set.getKey(), set.getValue()+(headsFound.contains(set.getKey()) ? 0 : 1));
+			}
+
+			List<Map.Entry<UUID, Integer>> sorted = new LinkedList<>(parsed.entrySet());
+			Comparator<Map.Entry<UUID, Integer>> valueComparator = Map.Entry.comparingByValue();
+			sorted.sort(valueComparator);
+
 			sender.sendMessage(ChatColor.RED + ""+ ChatColor.BOLD + "## Points ##");
 			sender.sendMessage("");
-			for (Map.Entry<String, Integer> set : sorted.entrySet()) {			
-				 sender.sendMessage(ChatColor.RED + set.getKey() + " - " + count.get(set.getKey()) 
-				 +(bcount.get(set.getKey())?ChatColor.GREEN + " (+1)":ChatColor.RED + " (+0)")
-				 );
-			 }
+			for (Map.Entry<UUID, Integer> set : sorted) {
+				sender.sendMessage(ChatColor.RED + SkinUtil.getName(set.getKey()) + " - " + count.get(set.getKey())
+						+ (!headsFound.contains(set.getKey()) ? ChatColor.GREEN + " (+1)" : ChatColor.RED + " (+0)")
+				);
+			}
 			break;
 		default:
-			sender.sendMessage(ChatColor.RED + "Usage: /heads [giveheads/start/stop/count]");
+			sender.sendMessage(ChatColor.RED + "Usage: /" + label + " [giveheads/start/stop/count]");
 			break;
 		}
 		return true;
---
+++ src/heads/inktest/util/SkinUtil.java
@@ -0,0 +1,130 @@
+package heads.inktest.util;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.UUID;
+
+/**
+ * Created by Rory on 6/25/2017.
+ */
+public class SkinUtil
+{
+	
+	private static final JsonParser parser = new JsonParser();
+	
+	private static final HashMap<String, String> skinCache = new HashMap<>();
+	private static final HashMap<UUID, String>   nameCache = new HashMap<>();
+	private static final HashMap<String, UUID> uuidCache = new HashMap<>();
+	
+	public static UUID getUUIDFromName(String name) {
+		return getUUIDFromName(name, false);
+	}
+	
+	public static UUID getUUIDFromName(String name, boolean checkAPI) {
+		if (uuidCache.containsKey(name.toLowerCase())) return uuidCache.get(name.toLowerCase());
+		
+		OfflinePlayer player = Bukkit.getPlayer(name);
+		if (player != null) {
+			uuidCache.putIfAbsent(player.getName().toLowerCase(), player.getUniqueId());
+			nameCache.putIfAbsent(player.getUniqueId(), player.getName());
+			return player.getUniqueId();
+		} else {
+			player = Bukkit.getOfflinePlayer(name);
+			
+			if (player.hasPlayedBefore()) {
+				uuidCache.putIfAbsent(player.getName().toLowerCase(), player.getUniqueId());
+				nameCache.putIfAbsent(player.getUniqueId(), player.getName());
+				return player.getUniqueId();
+			}
+		}
+		
+		if (checkAPI) {
+			try {
+				URL url_0 = new URL("https://api.mojang.com/users/profiles/minecraft/" + name);
+				InputStreamReader reader_0 = new InputStreamReader(url_0.openStream());
+				JsonObject json = SkinUtil.parser.parse(reader_0).getAsJsonObject();
+				
+				String uuidString = json.get("id").getAsString();
+				uuidString = uuidString.substring(0, 8) + "-" + uuidString.substring(8, 12) + "-" + uuidString.substring(12, 16) + "-" + uuidString.substring(16, 20) + "-" + uuidString.substring(20);
+				UUID uuid = UUID.fromString(uuidString);
+				
+				name = json.get("name").getAsString();
+				
+				uuidCache.put(name.toLowerCase(), uuid);
+				nameCache.put(uuid, name);
+				
+				return uuid;
+			} catch (Exception e) {
+				//
+			}
+		}
+		
+		return null;
+	}
+	
+	public static String getName(UUID uuid) {
+		return getName(uuid, false);
+	}
+	
+	public static String getName(UUID uuid, boolean checkAPI) {
+		if (nameCache.containsKey(uuid)) return nameCache.get(uuid);
+		
+		OfflinePlayer player = Bukkit.getPlayer(uuid);
+		if (player != null) {
+			uuidCache.put(player.getName().toLowerCase(), uuid);
+			nameCache.put(uuid, player.getName());
+			return player.getName();
+		} else {
+			player = Bukkit.getOfflinePlayer(uuid);
+			
+			if (player.hasPlayedBefore()) {
+				uuidCache.put(player.getName().toLowerCase(), uuid);
+				nameCache.put(uuid, player.getName());
+				return player.getName();
+			}
+		}
+		
+		if (checkAPI) {
+			try {
+				String uuidStr = uuid.toString().replace("-", "");
+				URL url_1 = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuidStr + "?unsigned=false");
+				InputStreamReader reader_1 = new InputStreamReader(url_1.openStream());
+				JsonObject jsonObject = new JsonParser().parse(reader_1).getAsJsonObject();
+				
+				String name = jsonObject.get("name").getAsString();
+				
+				uuidCache.put(name.toLowerCase(), uuid);
+				nameCache.put(uuid, name);
+				
+				return name;
+			} catch (Exception e) {
+				//
+			}
+		}
+		
+		return "NULL";
+	}
+	
+	public static String[] getValueAndSignature(UUID uuid) {
+		try {
+			String uuidStr = uuid.toString().replace("-", "");
+			URL url_1 = new URL("https://sessionserver.mojang.com/session/minecraft/profile/" + uuidStr + "?unsigned=false");
+			InputStreamReader reader_1 = new InputStreamReader(url_1.openStream());
+			JsonObject textureProperty = new JsonParser().parse(reader_1).getAsJsonObject().get("properties").getAsJsonArray().get(0).getAsJsonObject();
+			String texture = textureProperty.get("value").getAsString();
+			String signature = textureProperty.get("signature").getAsString();
+			String[] args = new String[2];
+			args[0] = texture;
+			args[1] = signature;
+			return args;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+}
